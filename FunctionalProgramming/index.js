e86a10cdfe5a4fca749666bc0ca87f5a5ffa912e
Samuel Chukwuemeka <88311214+samdom4peace@users.noreply.github.com>
2021-09-12 19:02:13 -0500

    Initial commit

---
+++ FunctionalProgramming/index.js
@@ -0,0 +1,102 @@
+// Copyright 2018 - Samuel Dominic Chukwuemeka (Samdom For Peace)
+// www.samuelchukwuemeka.com
+// www.chukwuemekasamuel.com
+// www.samdomforpeace.com
+// www.samuelchukwuemeka.com/Sequences/sequences.html
+// www.sequences.appspot.com
+// www.chukwuemeka-samuel.appspot.com
+
+//"use strict";
+
+
+// Solutions to the Questions
+function solutions(solution) {
+    var e = document.getElementById(solution);
+    if (e.style.display === 'block' || e.style.display ==='') e.style.display = 'none';
+    else e.style.display = 'block';
+}
+
+
+function toggle(id) {
+    var e = document.getElementById(id);
+    if (e.style.display === 'none')
+        e.style.display = 'inline-block';
+    else
+        e.style.display = 'none';
+}
+
+// Accordion 
+function sequencesOverview() {
+    var x = document.getElementById("overview");
+    if (x.className.indexOf("w3-show") === -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function sequencesArithmetic() {
+    var x = document.getElementById("arithmeticSequence");
+    if (x.className.indexOf("w3-show") === -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function sequencesGeometric() {
+    var x = document.getElementById("geometricSequence");
+    if (x.className.indexOf("w3-show") === -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function sequencesQuadratic() {
+    var x = document.getElementById("quadraticSequence");
+    if (x.className.indexOf("w3-show") === -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function sequencesRecursive() {
+    var x = document.getElementById("recursiveSequence");
+    if (x.className.indexOf("w3-show") === -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function sequencesOther() {
+    var x = document.getElementById("otherSequences");
+    if (x.className.indexOf("w3-show") === -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function sequencesAll() {
+    var x = document.getElementById("allSequences");
+    if (x.className.indexOf("w3-show") === -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+// Script to open and close sidebar
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# javascript-modules
+The repository is the template for subsequent JavaScript applications in the course learning modules. 
