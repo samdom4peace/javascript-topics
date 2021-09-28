2fdee98eef168255cefbfd8e2aeaa47705ea00dc
Samuel Chukwuemeka <88311214+samdom4peace@users.noreply.github.com>
2021-09-28 14:57:39 -0500

    Add files via upload

--- FunctionalProgramming/statements.js
+++ FunctionalProgramming/statements.js
@@ -138,3 +138,73 @@ switch (continent)
 }
 
 // Repetitive Statements
+
+// Example 1: Multiplication Tables of Numbers 3, 7, 12 using For Loop, While Loop, and Do-While Loop
+// Use: For-Loop for Number 3; While Loop for Number 7; Do-While Loop for Number 12
+// Declare the constants and the variable
+var firstMultiplier = 3, secondMultiplier = 7, thirdMultiplier = 12;
+var multiplicand;
+
+// For Loop for the Multiplication Table of Number 3
+for (multiplicand = 1; multiplicand <= 12; multiplicand++)
+{   document.getElementById("Table3").innerHTML += 
+    firstMultiplier + " * " + multiplicand + " = " + (firstMultiplier * multiplicand) + "<br>";
+}
+
+// While Loop for the Multiplication Table of Number 7
+multiplicand = 1;
+while (multiplicand <= 12)
+{
+    document.getElementById("Table7").innerHTML += 
+    secondMultiplier + " * " + multiplicand + " = " + (secondMultiplier * multiplicand) + "<br>";
+    multiplicand++;
+}
+
+// Do-While Loop for the Multiplication Table of Number 12
+multiplicand = 1;
+do
+{
+    document.getElementById("Table12").innerHTML +=
+    thirdMultiplier + " * " + multiplicand + " = " + (thirdMultiplier * multiplicand) + "<br>";
+    multiplicand++;
+}
+while (multiplicand <= 12);
+
+// Example 2: Print Number Types using Do-While Loop, While Loop and For Loop
+// Print on the same line. Separate each number with a comma
+// Declare the variable
+var number;
+
+// Do-While Loop: Print the first ten positive integers
+number = 1;
+do
+{
+    document.getElementById("positiveIntegers").innerHTML += number + ", ";
+    number++;
+}
+while (number <= 10);
+
+// While Loop: Print the even numbers between 1 and 20 (both ends are exclusive)
+number = 2;
+while (number < 20)
+{
+    if (number % 2 == 0)
+    {
+        document.getElementById("evenNumbers").innerHTML += number + ", ";
+    }
+    number++;
+}
+
+// For Loop: Print the perfect squares between 1 and 100 (both ends are inclusive)
+for (number = 1; number <= 100; number++)
+{
+    // Declare a variable for the perfect square
+    var squareNumber = Math.pow(number, 2);
+
+    if(squareNumber <= 100)
+    {
+        document.getElementById("perfectSquares").innerHTML += squareNumber + ", ";
+    }
+}
+
+// Branching Statements
